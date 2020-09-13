c442b6bad73d5d3c315579a6d9aabd230dce8b03
damu <damunano.1989@gmail.com>
2020-09-13 21:24:36 +0530

    Adding solution

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,13 @@
 # bounce.py
 #
 # Exercise 1.5
+def new_height(curr_height):
+    return 0.6*curr_height
+
+def main(height=100):
+    for i in range(11):
+        print(i,round(height,4))
+        height = new_height(height)
+
+if __name__ == "__main__":
+    main(100)
