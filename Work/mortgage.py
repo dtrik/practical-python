d4cfa013bf364180d2f6fc01e1b42609d0c3bcbc
damu <damunano.1989@gmail.com>
2020-09-13 23:18:04 +0530

    Solving E1.9

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -6,9 +6,15 @@ interest = 0.05
 monthly = 2684.11
 paid = 0
 month = 0
+extra_payment_start_month = 61
+extra_payment_end_month = 108
+extra_payment = 1000
+
 while principal > 0:
     month += 1
-    principal = principal * (1 + interest/12) - (monthly+1000*(month<=12))
-    paid += monthly+(1000*(month<=12))
-
+    per_month = monthly+extra_payment*(month in range(
+        extra_payment_start_month, 
+        extra_payment_end_month+1))
+    principal = principal * (1 + interest/12) - per_month
+    paid += per_month
 print('Total paid', paid, 'in', month)
