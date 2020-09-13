5e155d1b718fb82df4f180b791f5453fec37e9d5
damu <damunano.1989@gmail.com>
2020-09-13 23:05:53 +0530

    Solving E1.8

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -5,9 +5,10 @@ principal = 500000
 interest = 0.05
 monthly = 2684.11
 paid = 0
-
+month = 0
 while principal > 0:
-    principal = principal * (1 + interest/12) - monthly
-    paid += monthly
+    month += 1
+    principal = principal * (1 + interest/12) - (monthly+1000*(month<=12))
+    paid += monthly+(1000*(month<=12))
 
-print('Total paid', paid)
+print('Total paid', paid, 'in', month)
