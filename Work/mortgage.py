98d19af125f75f3d638e524c6d129f8e6c3875cb
damu <damunano.1989@gmail.com>
2020-09-13 22:55:10 +0530

    update mortgate.py for e1.7

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -1,3 +1,13 @@
 # mortgage.py
 #
 # Exercise 1.7
+principal = 500000
+interest = 0.05
+monthly = 2684.11
+paid = 0
+
+while principal > 0:
+    principal = principal * (1 + interest/12) - monthly
+    paid += monthly
+
+print('Total paid', paid)
