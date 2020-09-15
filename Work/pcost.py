4a61d251668433406b0ef2b167c03b538478c4c8
damu <damunano.1989@gmail.com>
2020-09-15 22:39:14 +0530

    Solved E1.27

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,12 @@
 # pcost.py
 #
 # Exercise 1.27
+total_cost = 0
+with open('Data/portfolio.csv', 'rt') as f:
+    next(f)
+    for line in f:
+        item = line.split(',')
+        num_stock = int(item[1])
+        cost_stock = float(item[2].strip('\n'))
+        total_cost += num_stock*cost_stock
+print(f'Total cost {total_cost}')
