26ea7e539443ecf6be16738d0ad64f730f9681a3
damu <damunano.1989@gmail.com>
2020-09-15 23:05:09 +0530

    Solved E1.30, converted to function

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,12 +1,20 @@
 # pcost.py
 #
 # Exercise 1.27
-total_cost = 0
-with open('Data/portfolio.csv', 'rt') as f:
-    next(f)
-    for line in f:
-        item = line.split(',')
-        num_stock = int(item[1])
-        cost_stock = float(item[2].strip('\n'))
-        total_cost += num_stock*cost_stock
-print(f'Total cost {total_cost}')
+def portfolio_cost(filename):
+    '''Returns the total cost of the portfolio in file
+    passed as input to the function'''
+    total_cost = 0
+    with open(filename, 'rt') as f:
+        next(f)
+        for line in f:
+            item = line.split(',')
+            num_stock = int(item[1])
+            cost_stock = float(item[2].strip('\n'))
+            total_cost += num_stock*cost_stock
+
+            #print(f'Total cost {total_cost}')
+    return total_cost
+
+cost = portfolio_cost('Data/portfolio.csv')
+print(f'Total cost: {cost}')
